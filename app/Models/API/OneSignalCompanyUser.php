0d43ecd765ece03e097892d3527d83fd8a6d9053
ftahery <ftahery93@gmail.com>
2019-06-18 11:06:28 +0300

    Adding fillables

--- app/Models/API/OneSignalCompanyUser.php
+++ app/Models/API/OneSignalCompanyUser.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class OneSignalCompanyUser extends Model
 {
     protected $table = "one_signal_company_users";
+    protected $fillable = ['company_id', 'player_id', 'device_type'];
 }
--- app/Models/API/OneSignalUser.php
+++ app/Models/API/OneSignalUser.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class OneSignalUser extends Model
 {
     protected $table = "one_signal_users";
+    protected $fillable = ['user_id', 'player_id', 'device_type'];
 }
