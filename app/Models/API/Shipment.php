654641a33943373f24512b6e85fa0329df239cae
ftahery <ftahery93@gmail.com>
2019-06-24 17:41:44 +0300

    added address id from and to in hidden

--- app/Models/API/Shipment.php
+++ app/Models/API/Shipment.php
@@ -9,7 +9,7 @@ class Shipment extends Model
 {
     protected $table = "shipments";
     protected $fillable = array('address_from_id', 'address_to_id', 'pickup_time', 'quantity', 'user_id', 'status', 'company_id', 'price', 'payment_type');
-    protected $hidden = array('name', 'image', 'description', 'created_at', 'updated_at', 'status');
+    protected $hidden = array('name', 'image', 'description', 'created_at', 'updated_at', 'status', 'address_from_id', 'address_to_id');
 
     public function categories()
     {
