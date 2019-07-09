1a1ab56d61f1f81e9be27dfd285f65f3a58fe28a
ftahery <ftahery93@gmail.com>
2019-07-09 13:17:13 +0300

    added company_id as fillable

--- app/Models/API/Order.php
+++ app/Models/API/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['shipment_id', 'free_deliveries', 'wallet_amount', 'card_amount', 'status'];
+    protected $fillable = ['shipment_id', 'free_deliveries', 'wallet_amount', 'card_amount', 'status', 'company_id'];
     protected $hidden = ['created_at', 'updated_at'];
 
     public function shipments()
