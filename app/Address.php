4a708b4695f2ff69580e597507d06d0462abf215
ftahery <ftahery93@gmail.com>
2019-07-22 15:49:28 +0300

    address response

--- app/Address.php
+++ app/Address.php
@@ -11,7 +11,7 @@ class Address extends Model
 {
     protected $table = "addresses";
     protected $fillable = array('name', 'block', 'street', 'country_id', 'city_id', 'governorate_id', 'building', 'notes', 'user_id', 'details', 'status', 'mobile');
-    protected $hidden = array('created_at', 'updated_at', 'user_id', 'status', 'country_id', 'governorate_id', 'city_id','area');
+    protected $hidden = array('created_at', 'updated_at', 'user_id', 'status', 'country_id', 'governorate_id', 'city_id', 'area');
     protected $appends = ['country', 'city', 'governorate'];
 
     public function city()
@@ -21,20 +21,20 @@ class Address extends Model
 
     public function getCOuntryAttribute()
     {
-        $country = Country::find($this->{'country_id'});
-        return $country->name;
+        return Country::find($this->{'country_id'});
+        //return $country->name;
     }
 
     public function getCityAttribute()
     {
-        $city = City::find($this->{'city_id'});
-        return $city->name;
+        return City::find($this->{'city_id'});
+        //return $city->name;
     }
 
     public function getGovernorateAttribute()
     {
-        $governorate = Governorate::find($this->{'governorate_id'});
-        return $governorate->name;
+        return Governorate::find($this->{'governorate_id'});
+        //return $governorate->name;
     }
 
 }
