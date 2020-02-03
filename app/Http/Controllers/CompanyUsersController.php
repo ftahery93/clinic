cab9b05a14e0c6eae5c7ac01daa37f9a82d331b1
Fakhruddin Tahery <ftaheryvavisa@gmail.com>
2020-02-03 17:07:38 +0300

    fixed company create controller

--- app/Http/Controllers/CompanyUsersController.php
+++ app/Http/Controllers/CompanyUsersController.php
@@ -112,6 +112,15 @@ class CompanyUsersController extends Controller
         $Company->status = 1;
         $Company->approved = 1;
         $Company->save();
+        
+        Wallet::create([
+            'company_id' =>  $Company->id,
+            'balance' => 0,
+        ]);
+        FreeDelivery::create([
+            'company_id' =>  $Company->id,
+            'quantity' => 0,
+        ]);
 
         return redirect()->action('CompanyUsersController@index')->with('doneMessage', trans('backend.addDone'));
     }
