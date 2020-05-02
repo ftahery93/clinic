cdcef73b2225dae7292a869fe60a60ad1279aa13
Fakhruddin <ftahery93@gmail.com>
2020-05-02 17:01:27 +0300

    changed pick up to boolean

--- app/Address.php
+++ app/Address.php
@@ -12,8 +12,8 @@ use App;
 class Address extends Model
 {
     protected $table = "addresses";
-    protected $fillable = array('name', 'block', 'street', 'country_id', 'city_id', 'governorate_id', 'building', 'notes', 'user_id', 'details', 'status', 'mobile', 'save', 'jeddah','title_id');
-    protected $hidden = array('created_at', 'updated_at', 'user_id', 'status', 'country_id', 'governorate_id', 'city_id', 'area', 'save','pivot');
+    protected $fillable = array('name', 'block', 'street', 'country_id', 'city_id', 'governorate_id', 'building', 'notes', 'user_id', 'details', 'status', 'mobile', 'save', 'jeddah', 'title_id', 'pick_up');
+    protected $hidden = array('created_at', 'updated_at', 'user_id', 'status', 'country_id', 'governorate_id', 'city_id', 'area', 'save', 'pivot');
     protected $appends = ['country', 'city', 'governorate', 'title'];
 
     public function city()
@@ -42,30 +42,33 @@ class Address extends Model
     public function getTitleAttribute()
     {
         $name = 'name_en';
-        if(App::getLocale() == 'ar'){
+        if (App::getLocale() == 'ar') {
             $name = 'name_ar';
         }
         $return = DB::table('address_titles')
-        ->select('id',$name.' AS name')
-        ->where('id',$this->{'title_id'})
-        ->first(); 
+            ->select('id', $name . ' AS name')
+            ->where('id', $this->{'title_id'})
+            ->first();
 
-        return $return;    
+        return $return;
     }
 
     public static function titles()
     {
         $name = 'name_en';
-        if(App::getLocale() == 'ar'){
+        if (App::getLocale() == 'ar') {
             $name = 'name_ar';
         }
-        return DB::table('address_titles')->select('id',$name.' AS name')->get();
+        return DB::table('address_titles')->select('id', $name . ' AS name')->get();
     }
 
     public function getPriceAttribute()
     {
-        return ($this->pivot['shipment_price'])?$this->pivot['shipment_price']:'0.000';
-        
+        return ($this->pivot['shipment_price']) ? $this->pivot['shipment_price'] : '0.000';
     }
 
+    public function getPickUpAttribute($value)
+    {
+        return $value ? true : false;
+    }
 }
